efbeea03df6bd315b83e2011e089d9e9ab1401e6
Lixquid <lixquid@lixquid.com>
2024-01-22 01:31:11 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1><a href="https://color-gradient-tool.lixquid.com">Color Gradient Tool</a></h1>
+
+A too to create, view, and use color gradients.
+
+https://color-gradient-tool.lixquid.com
+
+## Development
+
+### Prerequisites
+
+- [Node.js](https://nodejs.org/)
+- [Yarn](https://yarnpkg.com/)
+
+### Getting Started
+
+1. Clone the repository
+2. Install dependencies: `yarn install`
+3. Start the development server: `yarn start`
+4. Open http://localhost:8080 in your browser
+
+Changes to the source files will be automatically reloaded in the browser.
+
+### Building for Production
+
+1. Remove the `dist` folder:
+   - \*nix: `rm -rf dist`
+   - Windows: `rmdir /s /q dist`
+2. Build the project: `yarn build`
+3. The production files will be in the `dist` folder
+
+Production files can be served from any static file server.
---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+/** Toggles bootstrap theme between light and dark */
+function toggleDarkMode() {
+    const html = document.querySelector("html");
+    if (html !== null) {
+        html.dataset["bsTheme"] =
+            html.dataset["bsTheme"] === "dark" ? "light" : "dark";
+    }
+}
+
+export function App() {
+    return (
+        <div class="container mx-auto py-5">
+            <div class="d-flex justify-content-between align-items-center mb-5 flex-wrap">
+                <h1>Color Gradient Tool</h1>
+                <div>
+                    <button
+                        class="btn btn-outline-secondary me-2"
+                        onClick={toggleDarkMode}
+                        title="Toggle dark mode"
+                    >
+                        <i class="bi bi-moon-fill" />
+                    </button>
+                    <a
+                        href="https://lixquid.com"
+                        class="btn btn-outline-primary float-end"
+                    >
+                        <i class="bi bi-box-arrow-up-right me-2" />
+                        lixquid.com
+                    </a>
+                </div>
+            </div>
+            <div class="mt-5 text-end">
+                <a href="https://github.com/lixquid/app-color-gradient-tool">
+                    <i class="bi bi-box-arrow-up-right me-2" />
+                    Source code
+                </a>
+            </div>
+        </div>
+    );
+}
